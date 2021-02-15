14359285b6f86aeba600fad50d56277503a9f4e4
Maria22 <m.ushakova22@gmail.com>
2021-02-15 21:57:47 +0100

    -create todo element

--- dist/js/script.js
+++ dist/js/script.js
@@ -2,10 +2,40 @@
 
 var toggleButton = document.querySelector('.header_switch');
 var body = document.querySelector('body');
+var input = document.querySelector('input');
+var form = document.querySelector('form');
+var todoItems = []; // toggle dark\light mode 
+
 toggleButton.addEventListener('click', function () {
   if (body.classList.contains('light')) {
     body.classList.remove('light');
   } else {
     body.classList.add('light');
+  }
+}); //create new element
+
+function addNewItem(text) {
+  var todoItem = {
+    text: text,
+    id: new Date(),
+    checked: false
+  };
+}
+
+function addItemToArray(arr, item) {
+  arr.push(item);
+}
+
+form.addEventListener('submit', function (e) {
+  e.preventDefault();
+  var text = input.value.trim();
+  console.log(text);
+
+  if (text !== ' ') {
+    var todoItem = addNewItem(text);
+    console.log(todoItem);
+    addItemToArray(todoItems, todoItem);
+    console.log(todoItems);
+    input.value = ' ';
   }
 });
--- src/js/script.js
+++ src/js/script.js
@@ -1,11 +1,41 @@
 const toggleButton = document.querySelector('.header_switch');
 const body = document.querySelector('body');
+const input = document.querySelector('input');
+const form = document.querySelector('form');
+const todoItems = [];
 
+// toggle dark\light mode 
 toggleButton.addEventListener('click', () => {
     if (body.classList.contains('light')) {
         body.classList.remove('light');
     }
     else {
         body.classList.add('light');
+    }
+});
+
+//create new element
+function addNewItem(text) {
+    const todoItem = {
+        text,
+        id: new Date(),
+        checked: false
+    };
+}
+
+function addItemToArray(arr, item) {
+    arr.push(item);
+}
+
+form.addEventListener('submit', e => {
+    e.preventDefault();
+    const text = input.value.trim();
+    console.log(text);
+    if (text !== ' ') {
+        const todoItem = addNewItem(text);
+        console.log(todoItem);
+        addItemToArray(todoItems, todoItem);
+        console.log(todoItems);
+        input.value = ' ';
     }
 });
